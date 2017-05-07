0db92c44de67fdfb18360096a2e238170052b7aa
Sebastian Schmidt <schro.sb@gmail.com>
2017-05-07 13:59:32 +0200

    SearchPage: only replace the non-utf8 chars

--- lib/httplog.py
+++ lib/httplog.py
@@ -233,12 +233,7 @@ class SearchPage(BaseResource):
                     if sys.version_info.major == 3:
                         content.append(msg)
                     else:
-                        try:
-                            content.append(msg.decode("utf-8"))
-                        except UnicodeDecodeError:
-                            logging.warn("Encountered non-unicode character"
-                                         " in channel logs - ignoring message"
-                                         " for search page")
+                        content.append(msg.decode("utf-8"), 'replace')
             datestr = name.lstrip(self.channel + ".").rstrip(".yaml")
             try:
                 date = datetime.strptime(datestr, "%Y-%m-%d")

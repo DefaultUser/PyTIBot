f7d65604df148c33368417761ac2678cbbc0cdef
Sebastian Schmidt <schro.sb@gmail.com>
2019-06-15 17:00:52 +0200

    Support for several different markov corpi

--- lib/channelwatcher/markov.py
+++ lib/channelwatcher/markov.py
@@ -27,15 +27,38 @@ from util import filesystem as fs
 class MarkovChat(abstract.ChannelWatcher):
     def __init__(self, bot, channel, config):
         super(MarkovChat, self).__init__(bot, channel, config)
-        self.corpus = os.path.join(fs.adirs.user_config_dir, "markov",
-                                   self.channel, config["corpus"])
+        self.units = []
+        for elem in config:
+            corpus = os.path.join(fs.adirs.user_config_dir, "markov",
+                                  self.channel.lstrip("#"), elem["corpus"])
+            keywords = elem["keywords"]
+            chat_rate = elem.get("chat_rate", 0.1)
+            add_rate = elem.get("add_rate", 0.4)
+            self.units.append(MarkovUnit(self, corpus, keywords, chat_rate,
+                                         add_rate))
+
+    def msg(self, user, message):
+        for unit in self.units:
+            unit.handle_msg(message)
+
+    def send_msg(self, message):
+        self.bot.msg(self.channel, message)
+
+    def connectionLost(self, reason):
+        for unit in self.units:
+            unit.save_corpus()
+
+class MarkovUnit(object):
+    def __init__(self, parent, corpus, keywords, chat_rate, add_rate):
+        self.parent = parent
+        self.corpus = corpus
+        self.keywords = keywords
+        self.chat_rate = chat_rate
+        self.add_rate = add_rate
         if not os.path.isfile(self.corpus):
             raise IOError("No such file: {}".format(self.corpus))
         with open(self.corpus) as f:
             self.model = markovify.Text(f.read())
-        self.keywords = config["keywords"]
-        self.chat_rate = config.get("chat_rate", 0.1)
-        self.add_rate = config.get("add_rate", 0.4)
 
     def add_to_corpus(self, message):
         if not message.endswith("."):
@@ -43,15 +66,15 @@ class MarkovChat(abstract.ChannelWatcher):
         temp = markovify.Text(message)
         self.model = markovify.combine([self.model, temp])
 
-    def msg(self, user, message):
+    def handle_msg(self, message):
         if not(any([keyword in message.lower() for keyword in self.keywords])):
             return
         if random.random() < self.chat_rate:
-            self.bot.msg(self.channel, self.model.make_short_sentence(240))
-        if random.random() < self.add_rate and len(message.split()) > 5:
+            self.parent.send_msg(self.model.make_short_sentence(240))
+        if random.random() < self.add_rate and len(message.split()) > 3:
             self.add_to_corpus(message)
 
-    def connectionLost(self, reason):
+    def save_corpus(self):
         with open(self.corpus, "w") as f:
             f.write(self.model.rejoined_text.replace(".", ".\n"))
 

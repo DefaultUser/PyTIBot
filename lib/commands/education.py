339ece5bd3c8b2f2e8719b0e8c1cb9e82a08263c
Sebastian Schmidt <schro.sb@gmail.com>
2021-03-04 17:18:57 +0100

    Remove pypi command
    The API was deprecated and is no longer available.

--- lib/commands/education.py
+++ lib/commands/education.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # PyTIBot - IRC Bot using python and the twisted library
-# Copyright (C) <2015-2016>  <Sebastian Schmidt>
+# Copyright (C) <2015-2021>  <Sebastian Schmidt>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -45,53 +45,3 @@ list of choices"""
             result = formatting.colored("Invalid call - check the help", "red")
         bot.msg(channel, result)
 
-
-def search_pypi(bot):
-    """Search for python packages from PyPI - usage: (search|info) packages"""
-    _baseurl = "https://pypi.python.org/pypi/{}/json"
-    _maxlen = 400
-    _client = xmlrpclib.ServerProxy("https://pypi.python.org/pypi")
-
-    def _handle_search_results(results, channel):
-        packagenames = [item['name'] for item in results]
-        num_packages = len(packagenames)
-        if num_packages < 10:
-            show_result = ", ".join(packagenames)
-        else:
-            show_result = ", ".join(packagenames[:10]) + " ..."
-        bot.msg(channel, str(num_packages) + " packages found: " + show_result)
-
-    @defer.inlineCallbacks
-    def _handle_info_results(results, channel):
-        data = yield results.json()
-        name = data["info"]["name"].encode("utf-8")
-        author = data["info"]["author"].encode("utf-8")
-        description = data["info"]["description"].encode("utf-8")
-        description = description.replace("\n", " ")
-        if len(description) > _maxlen:
-            description = description[:_maxlen] + "..."
-        bot.msg(channel, "{} by {}: {}".format(formatting.bold(name, True),
-                                               formatting.bold(author, True),
-                                               description), length=510)
-        bot.msg(channel, data["info"]["package_url"].encode("utf-8"))
-
-    def _handle_error(error, arg, channel):
-        bot.msg(channel, "No such package: {}".format(arg))
-
-    while True:
-        args, sender, senderhost, channel = yield
-        if len(args) < 2 or not args[0] in ["search", "info"]:
-            bot.msg(channel, formatting.colored("Invalid call - check the help",
-                                                "red"))
-            continue
-
-        if args[0] == "search":
-            for package in args[1:]:
-                d = threads.deferToThread(_client.search, ({'name': package}))
-                d.addCallback(_handle_search_results, channel)
-        elif args[0] == "info":
-            for package in args[1:]:
-                url = _baseurl.format(package)
-                d = get(url)
-                d.addCallback(_handle_info_results, channel)
-                d.addErrback(_handle_error, package, channel)

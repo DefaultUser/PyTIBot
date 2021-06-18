7a3a3e5c018b73143b6e2842c4308ee28d08a2b5
Sebastian Schmidt <schro.sb@gmail.com>
2021-06-18 20:32:34 +0200

    Add "load_settings" to the IBot interface

--- backends/interfaces.py
+++ backends/interfaces.py
@@ -65,6 +65,9 @@ class IBot(zope.interface.Interface):
     def is_user_admin(user):
         """Check if a user is an admin for the bot"""
 
+    def load_settings():
+        """(Re)load settings from config"""
+
     def quit(message=None):
         """Quit the bot"""
 

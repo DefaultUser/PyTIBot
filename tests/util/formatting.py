0518db667d56597497b6e7b616e6324ecd5692f0
Sebastian Schmidt <schro.sb@gmail.com>
2023-03-10 17:21:53 +0100

    Formatting: Add a html/matrix parser
    The parser will drop all tags and attributes it deems unsafe.
    The output of this parser is a `twisted.web.template.Tag`, which will be
    used as internal string representation.
    The parser can also optionally parse `t:slot` and `t:attr`.

---
+++ tests/util/formatting.py
@@ -0,0 +1,121 @@
+from twisted.trial import unittest
+
+from typing import Any
+
+from twisted.web.template import Tag, tags, slot
+
+from util.formatting import ColorCodes
+from util.formatting import html as htmlformatting
+
+
+def _compare_tags(testcase: unittest.TestCase, item1: Any, item2: Any):
+    testcase.assertEqual(type(item1), type(item2))
+    if isinstance(item1, slot):
+        testcase.assertEqual(item1.name, item2.name)
+    elif not isinstance(item1, Tag):
+        testcase.assertEqual(item1, item2)
+    else:
+        testcase.assertEqual(item1.tagName, item2.tagName)
+        testcase.assertEqual(item1.attributes.keys(), item2.attributes.keys())
+        for key in item1.attributes.keys():
+            _compare_tags(testcase, item1.attributes[key],
+                          item2.attributes[key])
+        testcase.assertEqual(item1.slotData, item2.slotData)
+        testcase.assertEqual(len(item1.children), len(item2.children))
+        for child_of_item1, child_of_item2 in zip(item1.children,
+                                                  item2.children):
+            _compare_tags(testcase, child_of_item1, child_of_item2)
+
+def _compare_styled_string(testcase: unittest.TestCase, l1: list[Tag|str],
+                           l2: list[Tag|str]):
+    testcase.assertEqual(len(l1), len(l2))
+    for item1, item2 in zip(l1, l2):
+        _compare_tags(testcase, item1, item2)
+
+
+class HTMLParserTestCase(unittest.TestCase):
+    def _test_parser(self, input_value, expected_outcome):
+        parse_result = htmlformatting.parse_html(input_value)
+        _compare_tags(self, parse_result, expected_outcome)
+
+    def _test_parser_with_slots(self, input_value, expected_outcome):
+        parse_result = htmlformatting.parse_html(input_value, allow_slots=True)
+        _compare_tags(self, parse_result, expected_outcome)
+
+    def test_simple_string(self):
+        self._test_parser("foo", Tag("")("foo"))
+
+    def test_simple_bold(self):
+        self._test_parser("<b>foo</b>", Tag("")(tags.b("foo")))
+        self._test_parser("foo<b>bar</b>", Tag("")("foo", tags.b("bar")))
+
+    def test_simple_italic(self):
+        self._test_parser("<i>foo</i>", Tag("")(tags.i("foo")))
+        self._test_parser("foo<i>bar</i>", Tag("")("foo", tags.i("bar")))
+
+    def test_simple_underline(self):
+        self._test_parser("<u>foo</u>", Tag("")(tags.u("foo")))
+        self._test_parser("foo<u>bar</u>", Tag("")("foo", tags.u("bar")))
+
+    def test_simple_strike(self):
+        self._test_parser("<del>foo</del>", Tag("")(Tag("del")("foo")))
+        self._test_parser("foo<del>bar</del>", Tag("")("foo", Tag("del")("bar")))
+
+    def test_simple_color(self):
+        self._test_parser('<font color="red">foo</font>',
+                          Tag("")(tags.font("foo", color=ColorCodes.red)))
+        self._test_parser('<font color="darkgreen">foo</font>',
+                          Tag("")(tags.font("foo", color=ColorCodes.dark_green)))
+        self._test_parser('<font color="darkorange">foo</font>',
+                          Tag("")(tags.font("foo", color=ColorCodes.dark_yellow)))
+        self._test_parser('<font color="silver">foo</font>',
+                          Tag("")(tags.font("foo", color="#c0c0c0")))
+        self._test_parser('<font color="#123">foo</font>',
+                          Tag("")(tags.font("foo", color="#123")))
+        self._test_parser('<font data-mx-color="red">foo</font>',
+                          Tag("")(tags.font("foo", color=ColorCodes.red)))
+        self._test_parser('<font background-color="red">foo</font>',
+                          Tag("")(tags.font("foo",
+                                            **{"background-color": ColorCodes.red})))
+        self._test_parser('<font data-mx-bg-color="red">foo</font>',
+                          Tag("")(tags.font("foo",
+                                            **{"background-color": ColorCodes.red})))
+
+    def test_replace_css_style(self):
+        self._test_parser('<span style="color:red">foo</span>',
+                          Tag("")(tags.span("foo", color=ColorCodes.red)))
+        self._test_parser('<span style="background-color:red">foo</span>',
+                          Tag("")(tags.span("foo",
+                                            **{"background-color": ColorCodes.red})))
+        self._test_parser('<div style="text-decoration:line-through">foo</div>',
+                          Tag("")(tags.div("foo", strike=True)))
+        self._test_parser('<div style="text-decoration:underline">foo</div>',
+                          Tag("")(tags.div("foo", underline=True)))
+        self._test_parser('<div style="font-weight: bold">foo</div>',
+                          Tag("")(tags.div("foo", bold=True)))
+        self._test_parser('<div style="font-style: italic">foo</div>',
+                          Tag("")(tags.div("foo", italic=True)))
+
+    def test_img_tag(self):
+        self._test_parser('<img src="example.com">',
+                          Tag("")("inline image src='example.com'"))
+
+    def test_drops_unsupported_tags(self):
+        self._test_parser('<script>alert("hax")</script>',
+                          Tag("")('alert("hax")'))
+
+    def test_slots(self):
+        self._test_parser('foo<t:slot name="slt"/>bar',
+                          Tag("")("foo", "bar"))
+        self._test_parser_with_slots('foo<t:slot name="slt"/>bar',
+                                     Tag("")("foo", slot("slt"), "bar"))
+
+    def test_attr_slot(self):
+        self._test_parser('<font><t:attr name="color"><t:slot name="slt" />'
+                          '</t:attr>bar</font>',
+                          Tag("")(tags.font("bar")))
+        self._test_parser_with_slots('<font><t:attr name="color">'
+                                     '<t:slot name="slt" /></t:attr>bar</font>',
+                                     Tag("")(tags.font("bar",
+                                                       color=Tag("")(slot("slt")))))
+
--- util/formatting/html.py
+++ util/formatting/html.py
@@ -14,8 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import deque
 from html import escape as htmlescape
-from twisted.web.template import tags, Tag
+from html import parser as htmlparser
+from twisted.web.template import tags, Tag, slot
 
 from util.formatting.common import ColorsHex, Style, StyledTextFragment, url_pat
 from util.formatting.irc import _extract_irc_style
@@ -173,6 +175,168 @@ HTMLColors = {
         "whitesmoke": "#f5f5f5",
         "yellowgreen": "#9acd32"
         }
+
+
+class HTMLParseError(Exception):
+    pass
+
+
+class SanetizingHTMLParser(htmlparser.HTMLParser):
+    """
+    Parser for html and matrix messages that restricts the used tags and
+    attributes to safe values. It additionally allows `<rainbow>` tags for a
+    rainbow colored font.
+    The parameter `allow_slots` defines whether `<t:slot>` and `<t:attr>` tags
+    shall be ignored. Their usage is according to twisted's web templates.
+    """
+    # https://spec.matrix.org/v1.5/client-server-api/#mroommessage-msgtypes
+    # Allowed tags:
+    # font, del, h1, h2, h3, h4, h5, h6, blockquote, p, a, ul, ol, sup, sub,
+    # li, b, i, u, strong, em, strike, code, hr, br, div, table, thead,
+    # tbody, tr, th, td, caption, pre, span, img, details, summary
+    # mx-reply tag is allowed at the beginning of a message
+    # Allowed attrs:
+    # data-mx-bg-color, data-mx-color, color
+    # TODO: support tables, (un)ordered lists, quotes and code/preformatted blocks
+
+    allowed_tags_with_attrs = ("font", "p", "div", "span")
+    allowed_tags = ("b", "strong", "u", "i", "em", "cite", "del", "strike", "s",
+                    "font", "a", "br", "span", "div", "img", "rainbow",
+                    "t:slot", "t:attr")
+
+
+    unpaired_tags = ("br", "hr", "img")
+
+    def __init__(self, *, allow_slots=False, convert_charrefs=True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.root = Tag("")
+        self._stack = deque([self.root])
+        self._allow_slots = allow_slots
+
+    @staticmethod
+    def parse_html_color(color: str) -> ColorCodes | str:
+        if color == "darkorange":
+            return ColorCodes.dark_yellow
+        try:
+            temp = ColorCodes[color.replace("dark", "dark_")]
+            return temp
+        except KeyError:
+            color = HTMLColors.get(color, color)
+            return color
+
+    @staticmethod
+    def parse_style(style: str) -> Style:
+        result = Style()
+        for line in style.split(";"):
+            tokens = line.split(":")
+            if len(tokens) != 2:
+                print("invalid inline style")
+                continue
+            prop = tokens[0].strip()
+            value = tokens[1].strip()
+            if prop == "text-decoration" and value == "underline":
+                result.underline = True
+            elif prop == "text-decoration" and value == "line-through":
+                result.strike = True
+            elif prop == "font-weight" and value == "bold":
+                result.bold = True
+            elif prop == "font-style" and value == "italic":
+                result.italic = True
+            elif prop == "color":
+                result.fg = SanetizingHTMLParser.parse_html_color(value)
+            elif prop == "background-color":
+                result.bg = SanetizingHTMLParser.parse_html_color(value)
+        return result
+
+    def handle_starttag(self, tagName: str, attrs: list[tuple[str, str]]):
+        if tagName not in SanetizingHTMLParser.allowed_tags:
+            return
+        # a new 'p' tag can close the previous one implicitly
+        if tagName == "p":
+            if self._stack[-1].tagName == "p":
+                self._stack.pop()
+        attrs = dict(attrs)
+        sanetized_attrs = {}
+        if tagName == "t:slot":
+            if self._allow_slots:
+                new_tag = slot(attrs["name"])
+                self._stack[-1].children.append(new_tag)
+            return
+        if tagName == "t:attr":
+            if self._allow_slots:
+                new_tag = Tag("")
+                self._stack[-1].attributes[attrs["name"]] = new_tag
+                self._stack.append(new_tag)
+            return
+        if tagName == "img":
+            src = attrs.get("src", None)
+            self.handle_data(f"inline image {src=}")
+            return
+        if tagName in SanetizingHTMLParser.allowed_tags_with_attrs:
+            if "style" in attrs:
+                style = SanetizingHTMLParser.parse_style(attrs["style"])
+                if style.bold:
+                    sanetized_attrs["bold"] = True
+                if style.italic:
+                    sanetized_attrs["italic"] = True
+                if style.underline:
+                    sanetized_attrs["underline"] = True
+                if style.strike:
+                    sanetized_attrs["strike"] = True
+                if style.fg:
+                    sanetized_attrs["color"] = style.fg
+                if style.bg:
+                    sanetized_attrs["background-color"] = style.bg
+            if ((color:=attrs.get("color", None)) or
+                    (color:=attrs.get("data-mx-color", None))):
+                color = SanetizingHTMLParser.parse_html_color(color)
+                sanetized_attrs["color"] = color
+            if ((color:=attrs.get("background-color", None)) or
+                    (color:=attrs.get("data-mx-bg-color", None))):
+                color = SanetizingHTMLParser.parse_html_color(color)
+                sanetized_attrs["background-color"] = color
+        if tagName == "a" and (href:=attrs.get("href", None)):
+            sanetized_attrs["href"] = href
+        new_tag = Tag(tagName)
+        new_tag.attributes = sanetized_attrs
+        self._stack[-1].children.append(new_tag)
+        if tagName not in SanetizingHTMLParser.unpaired_tags:
+            self._stack.append(new_tag)
+
+    def handle_data(self, data: str):
+        self._stack[-1].children.append(data)
+
+    def handle_endtag(self, tagName: str):
+        if tagName not in SanetizingHTMLParser.allowed_tags:
+            return
+        if tagName in SanetizingHTMLParser.unpaired_tags:
+            # these tags can have an end tag, but don't have to depending on
+            # which version of HTML is used
+            return
+        if tagName == "t:slot":
+            return
+        if tagName == "t:attr":
+            if not self._allow_slots:
+                return
+            tagName = ""
+        #if tagName in ("ol", "ul") and self._stack[-1].tagName == "li":
+        #    self._stack.pop()
+        expected_tag = self._stack.pop()
+        if tagName != expected_tag.tagName:
+            raise HTMLParseError(f"Invalid HTML detected: Expected end of tag "
+                                 f"'{expected_tag.tagName}', but got '{tagName}'")
+
+    def close(self):
+        super().close()
+        if len(self._stack) != 1:
+            raise HTMLParseError("Unclosed tag detected")
+
+
+def parse_html(data: str, allow_slots=False) -> Tag:
+    p = SanetizingHTMLParser(allow_slots=allow_slots)
+    p.feed(data)
+    p.close()
+    return p.root
 
 
 def _style_html_string(style: Style) -> str:

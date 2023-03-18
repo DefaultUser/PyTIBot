90e6441a6945203cd3d00b18b917a52c350492a1
Sebastian Schmidt <schro.sb@gmail.com>
2023-03-18 20:39:22 +0100

    Add typehints to backend interfaces

--- backends/interfaces.py
+++ backends/interfaces.py
@@ -14,63 +14,66 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Optional
 import zope.interface
 
-
-class IBotProvider(zope.interface.Interface):
-    """Interface for objects that provide access to the running bot instance"""
-    bot = zope.interface.Attribute("""The bot instance""")
-
-    def get_bot():
-        """Getter for the bot instance"""
+from util.formatting import Message
 
 
 class IBot(zope.interface.Interface):
     """Interface for all Bot backends"""
-    userlist = zope.interface.Attribute("""Dictionary containing all users per channel""")
+    userlist: dict[str,list[str]] = zope.interface.Attribute("""Dictionary containing all users per channel""")
 
-    def setNick(newnick):
+    def setNick(newnick: str):
         """Set a new nickname"""
 
-    def msg(target, message, length=None):
+    def msg(target: str, message: Message, length: Optional[int]=None):
         """Send a message to a channel/room/user"""
 
-    def notice(target, message, length=None):
+    def notice(target: str, message: Message, length: Optional[int]=None):
         """Send a notice to a channel/room/user"""
 
-    def describe(channel, action):
+    def describe(channel: str, action: str):
         """Do an action/emote"""
 
-    def join(channel):
+    def join(channel: str):
         """Join a channel"""
 
-    def leave(channel):
+    def leave(channel: str):
         """Leave a channel"""
 
-    def kick(channel, user, reason=""):
+    def kick(channel: str, user: str, reason: str=""):
         """Kick a user from a channel"""
 
-    def ban(channel, user, reason=""):
+    def ban(channel: str, user: str, reason: str=""):
         """Ban a user from a channel"""
 
-    def enable_command(cmd, name, add_to_config=False):
+    def enable_command(cmd: str, name: str, add_to_config: bool=False):
         """Enable a command"""
 
-    def enable_trigger(trigger, add_to_config=False):
+    def enable_trigger(trigger: str, add_to_config: bool=False):
         """Enable a trigger"""
 
-    def ignore_user(user):
+    def ignore_user(user: str) -> bool:
         """Test whether to ignore a user"""
 
-    def get_auth(user):
+    def get_auth(user: str) -> str:
         """Get a users auth"""
 
-    def is_user_admin(user):
+    def is_user_admin(user: str) -> bool:
         """Check if a user is an admin for the bot"""
 
     def reload():
         """(Re)load settings from config"""
 
-    def quit(message=None):
+    def quit(message: Optional[str]=None):
         """Quit the bot"""
+
+
+class IBotProvider(zope.interface.Interface):
+    """Interface for objects that provide access to the running bot instance"""
+    bot: IBot = zope.interface.Attribute("""The bot instance""")
+
+    def get_bot() -> Optional[IBot]:
+        """Getter for the bot instance"""
 

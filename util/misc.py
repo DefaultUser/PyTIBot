c820403faf5b89e2760a038f4f25ad85c6a1e009
Sebastian Schmidt <schro.sb@gmail.com>
2023-05-12 17:27:22 +0200

    Code cleanup
    Convert a few strings into raw strings for regex.

--- util/misc.py
+++ util/misc.py
@@ -54,7 +54,7 @@ def filter_dict(data, rule):
     Returns True if rule applies to the dictionary
     """
     def _f(fragment, subdata):
-        key_path, cmp, val = re.split("\s*(==|!=)\s*", fragment, maxsplit=1)
+        key_path, cmp, val = re.split(r"\s*(==|!=)\s*", fragment, maxsplit=1)
         temp = subdata
         key_path = key_path.split(".")
         for path_index, key_frag in enumerate(key_path):
@@ -64,7 +64,7 @@ def filter_dict(data, rule):
                 else:
                     # otherwise it's a dict
                     star_replacements = temp.keys()
-                return all(_f(".".join([star, *key_path[path_index+1:]])+cmp+val, temp)
+                return all(_f(".".join([star, *key_path[path_index + 1:]]) + cmp + val, temp)
                            for star in star_replacements)
             if isinstance(temp, list) and key_frag.isnumeric():
                 key_frag = int(key_frag)
@@ -72,11 +72,11 @@ def filter_dict(data, rule):
         # values from rules are always strings
         temp = str(temp)
         if cmp == "!=":
-            return all(not fnmatch(temp, v) for v in re.split("\s*\|\s*", val))
-        return any(fnmatch(temp, v) for v in re.split("\s*\|\s*", val))
+            return all(not fnmatch(temp, v) for v in re.split(r"\s*\|\s*", val))
+        return any(fnmatch(temp, v) for v in re.split(r"\s*\|\s*", val))
 
     try:
-        if all(map(_f, re.split("\s+AND\s+", rule), itertools.repeat(data))):
+        if all(map(_f, re.split(r"\s+AND\s+", rule), itertools.repeat(data))):
             return True
     except Exception as e:
         logger.warn("Filter rule '{rule}' couldn't be applied: {e}",
